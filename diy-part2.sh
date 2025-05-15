4913c132888702042dec5f401f38dd31a10b7dad
Wei Chen <wei.chen@streamunlimited.com>
2025-05-15 17:01:09 +0800

    apply patch for mwlwifi to fix the compile errors

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,7 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+cp ../patches/mwlwifi/001-Fix-compilation-warning-with-64-bit-system.patch package/kernel/mwlwifi/patches/
+cp ../patches/mwlwifi/006-remove-uaccess-and-get_fs-calls-from-PCIe-for-Kenel-.patch package/kernel/mwlwifi/patches/
+cp ../patches/mwlwifi/007-replace-usage-of-the-deprecated-pci-dma-compat.h-API.patch package/kernel/mwlwifi/patches/
